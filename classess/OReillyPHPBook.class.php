21439d8dd4303204308824d2c1c66c9bcd5eaf4b
Sumit Chikane <sumitschikane@gmail.com>
2018-06-01 11:21:46 +0000

    OReillyPHPBook.class.php created online with Bitbucket

---
+++ classess/OReillyPHPBook.class.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ *   OReillyPHPBook classes
+ */
+class OReillyPHPBook extends AbstractPHPBook {
+    private $author;
+    private $title;
+    private static $oddOrEven = 'odd';
+    function __construct()
+    {
+        //alternate between 2 books
+        if ('odd' == self::$oddOrEven) {
+            $this->author = 'Rasmus Lerdorf and Kevin Tatroe';
+            $this->title = 'Programming PHP';
+            self::$oddOrEven = 'even';
+        }
+        else {
+            $this->author = 'David Sklar and Adam Trachtenberg';
+            $this->title = 'PHP Cookbook';
+            self::$oddOrEven = 'odd';
+        }
+    }
+    function getAuthor() {
+        return $this->author;
+    }
+    function getTitle() {
+        return $this->title;
+    }
+}
