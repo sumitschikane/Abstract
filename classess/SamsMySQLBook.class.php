27a77c06880af4ba66c8b6ff411516c578c626b4
Sumit Chikane <sumitschikane@gmail.com>
2018-06-01 11:22:44 +0000

    SamsMySQLBook.class.php created online with Bitbucket

---
+++ classess/SamsMySQLBook.class.php
@@ -0,0 +1,19 @@
+<?php 
+
+/*
+ *   SamsMySQLBook classes
+ */
+class SamsMySQLBook extends AbstractMySQLBook {
+    private $author;
+    private $title;
+    function __construct() {
+        $this->author = 'Paul Dubois';
+        $this->title = 'MySQL, 3rd Edition';
+    }
+    function getAuthor() {
+        return $this->author;
+    }
+    function getTitle() {
+        return $this->title;
+    }
+}
